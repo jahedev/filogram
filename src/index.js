e6c5e24065f61cc6ff548c48439680dd86ef5da9
Jahed Hossain <jahedev@gmail.com>
2021-09-09 15:16:12 -0400

    login form is almost complete, accidentally called everything as signup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import SignUp from './SignUp'
 import './assets/css/styles.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignUp />
   </React.StrictMode>,
   document.getElementById('root')
 )
