8c6e1e4018dc92e84641d4bcd0d7f73e4823e007
jahedev <jahedev@gmail.com>
2022-01-01 14:44:34 -0500

    coded profile-top

--- src/Profile.js
+++ src/Profile.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 import Header from './components/base/Header'
-import Stories from './components/home/Stories'
-import Posts from './components/home/Posts'
 
-import profile from './assets/images/profile.jpg'
-import ProfileView from './components/home/ProfileView'
-import FollowSuggestions from './components/home/FollowSuggestions'
-import Footer from './components/base/Footer'
+import profile from './assets/images/profile5.jpg'
 
 export default class Profile extends Component {
   render() {
@@ -15,9 +11,34 @@ export default class Profile extends Component {
       <div id='homepage'>
         <Header filled={{ profile: true }} />
         <main>
-          <div className='center-middle flex-column'>
+          <div className='center-middle flex-column profile-page-container'>
             <div className='profile-top'>
-              <p>hello</p>
+              <div className='profile-photo pp-large'>
+                <img src={profile} alt='' />
+              </div>
+              <div className='profile-info'>
+                <div className='profile-info-top'>
+                  <h2 id='profile-username'>mojahos</h2>
+                  <Link className='button' to='/'>
+                    Edit Profile
+                  </Link>
+                </div>
+                <div className='profile-info-middle'>
+                  <span id='profile-post-count'>
+                    <strong>3</strong>&nbsp;posts
+                  </span>
+                  <span id='profile-follower-count'>
+                    <strong>59</strong>&nbsp;followers
+                  </span>
+                  <span id='profile-following-count'>
+                    <strong>95</strong>&nbsp;following
+                  </span>
+                </div>
+                <div className='profile-info-bottom'>
+                  <h4 id='profile-name'>Jahed Hossain</h4>
+                  <span id='profile-description'>non-conforming conformer</span>
+                </div>
+              </div>
             </div>
             <div className='profile-bottom'>
               <p>world</p>
