f3078c38f6da3742e138cc687b25bc668dc6dfa6
Jahed Hossain <jahedev@gmail.com>
2022-01-01 12:50:18 -0500

    started creating profile page

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Header from './components/base/Header'
 import Stories from './components/home/Stories'
@@ -9,30 +9,32 @@ import ProfileView from './components/home/ProfileView'
 import FollowSuggestions from './components/home/FollowSuggestions'
 import Footer from './components/base/Footer'
 
-export default function Home() {
-  return (
-    <div id='homepage'>
-      <Header filled={{ home: true }} />
-      <main>
-        <div className='center-middle'>
-          <div className='main-left'>
-            <Stories />
-            <Posts />
+export default class Home extends Component {
+  render() {
+    return (
+      <div id='homepage'>
+        <Header filled={{ home: true }} />
+        <main>
+          <div className='center-middle'>
+            <div className='main-left'>
+              <Stories />
+              <Posts />
+            </div>
+            <div className='main-right'>
+              <ProfileView
+                largeSize={true}
+                image={profile}
+                username={'jahedev'}
+                subtitle={'Jahed Hossain'}
+                actionText={'Switch'}
+                actionHandler={'INSERT CALLBACK'}
+              />
+              <FollowSuggestions />
+              <Footer />
+            </div>
           </div>
-          <div className='main-right'>
-            <ProfileView
-              largeSize={true}
-              image={profile}
-              username={'jahedev'}
-              subtitle={'Jahed Hossain'}
-              actionText={'Switch'}
-              actionHandler={'INSERT CALLBACK'}
-            />
-            <FollowSuggestions />
-            <Footer />
-          </div>
-        </div>
-      </main>
-    </div>
-  )
+        </main>
+      </div>
+    )
+  }
 }
